c80a73ff294960f17cdc07c7c52fee6c97c21a6f
Alejandro <amogalla@iesrafaelalberti.es>
2022-02-25 17:53:54 +0100

    Modificada la imagen de los asteroides.

--- src/main/kotlin/AsteroidComponent.kt
+++ src/main/kotlin/AsteroidComponent.kt
@@ -1,5 +1,8 @@
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -8,6 +11,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.imageFromResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -21,4 +27,24 @@ fun Asteroid(asteroidData: AsteroidData) {
             .clip(CircleShape)
             .background(Color(102, 102, 153))
     )
+    {
+        Canvas(modifier = Modifier.fillMaxSize(), onDraw = {
+            drawPath(
+                color = Color.Transparent,
+                path = Path().apply {
+                    val size = asteroidSize.toPx()
+                    moveTo(0f, 0f) // Top-left corner...
+                    lineTo(size, size / 2f) // ...to right-center...
+                    lineTo(0f, size) // ... to bottom-left corner.
+                }
+            )
+        })
+        val imageModifier = Modifier
+        Image(
+            bitmap = imageFromResource("asteroid.png"),
+            "image",
+            imageModifier,
+            contentScale = ContentScale.Fit
+        )
+    }
 }
