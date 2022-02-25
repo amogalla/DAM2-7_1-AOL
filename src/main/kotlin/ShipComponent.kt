adff2eae891726543f05cac28ac7beeefecc3c98
Alejandro <amogalla@iesrafaelalberti.es>
2022-02-25 13:46:41 +0100

    Modificada la imagen de la nave.

--- src/main/kotlin/ShipComponent.kt
+++ src/main/kotlin/ShipComponent.kt
@@ -5,10 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
@@ -24,12 +22,12 @@ fun Ship(shipData: ShipData) {
             .offset(shipData.xOffset, shipData.yOffset)
             .size(shipSize)
             .rotate(shipData.visualAngle.toFloat())
-            .clip(CircleShape)
-            .background(Color.Black)
+            //.clip(CircleShape)
+            .background(Color.Transparent)
     ) {
         Canvas(modifier = Modifier.fillMaxSize(), onDraw = {
             drawPath(
-                color = Color.White,
+                color = Color.Transparent,
                 path = Path().apply {
                     val size = shipSize.toPx()
                     moveTo(0f, 0f) // Top-left corner...
